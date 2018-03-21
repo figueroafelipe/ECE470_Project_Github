79d5835f2b02e2b13c78703f3c54a7c098d00fc4
Felipe Figueroa <figueroafelipe@hotmail.com>
2018-03-21 18:27:14 -0500

    created project functions file and polished up assignment 3. Created holder file for assignment4

--- ECE470_Project_Code.py/ECE470_Project_Functions.py
+++ ECE470_Project_Code.py/ECE470_Project_Functions.py
@@ -32,8 +32,65 @@ def forward_kinematics (theta1,theta2,theta3,theta4,theta5,theta6):
     d = np.dot(c, linalg.expm(S5_skew * theta5))
     e = np.dot(d, linalg.expm(S6_skew * theta6))
     T_1in0 = np.dot(e,M)
-    return T_1in0
 
+    R_1in0 = np.array(T_1in0[0:3, 0:3])
+    eulerAngles = rotationMatrixToEulerAngles(R_1in0)
+    p_1in0 = np.array(T_1in0[0:3, 3])
+
+    print('\nPose of end-effector: ')
+    print(T_1in0, end='\n\n')
+    print('Euler Angles: ')
+    print(eulerAngles, end='\n\n')
+
+
+    return (T_1in0, R_1in0, eulerAngles, p_1in0)
+
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R):
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
+def rotationMatrixToEulerAngles(R):
+    assert (isRotationMatrix(R))
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
+
+def start_simulation():
+    # Close all open connections (just in case)
+    vrep.simxFinish(-1)
+
+    # Connect to V-REP (raise exception on failure)
+    clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+    if clientID == -1:
+        raise Exception('Failed connecting to remote API server')
+
+    # Start simulation
+    vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
+
+    # Wait two seconds
+    time.sleep(2)
+
+    return clientID
 
 def move_robot (clientID, position, eulerAngles, theta1, theta2, theta3, theta4, theta5, theta6):
 
@@ -91,50 +148,51 @@ def move_robot (clientID, position, eulerAngles, theta1, theta2, theta3, theta4,
 
     # Position all the joint angles according to user input
     # Set the desired value of the first joint variable
-    t = time.process_time()
     vrep.simxSetJointTargetPosition(clientID, joint_one_handle, theta1, vrep.simx_opmode_oneshot)
-    t1 = time.process_time() - t
-    # t1 = vrep.simxGetLastCmdTime(clientID)
-    # print(t1)
     time.sleep(2)  # Wait two seconds
 
     # Set the desired value of the second joint variable
-    t = time.process_time()
     vrep.simxSetJointTargetPosition(clientID, joint_two_handle, theta2, vrep.simx_opmode_oneshot)
-    t2 = time.process_time() - t
-    # t2 = vrep.simxGetLastCmdTime(clientID)
     time.sleep(2)  # Wait two seconds
 
     # Set the desired value of the third joint variable
-    t = time.process_time()
     vrep.simxSetJointTargetPosition(clientID, joint_three_handle, theta3, vrep.simx_opmode_oneshot)
-    t3 = time.process_time() - t
-    # t3 = vrep.simxGetLastCmdTime(clientID)
     time.sleep(2)  # Wait two seconds
 
     # Set the desired value of the fourth joint variable
-    t = time.process_time()
     vrep.simxSetJointTargetPosition(clientID, joint_four_handle, theta4, vrep.simx_opmode_oneshot)
-    t4 = time.process_time() - t
-    # t4 = vrep.simxGetLastCmdTime(clientID)
     time.sleep(2)  # Wait two seconds
 
     # Set the desired value of the fifth joint variable
-    t = time.process_time()
     vrep.simxSetJointTargetPosition(clientID, joint_five_handle, theta5, vrep.simx_opmode_oneshot)
-    t5 = time.process_time() - t
-    # t5 = vrep.simxGetLastCmdTime(clientID)
     time.sleep(2)  # Wait two seconds
 
     # Set the desired value of the sixth joint variable
-    t = time.process_time()
     vrep.simxSetJointTargetPosition(clientID, joint_six_handle, theta6, vrep.simx_opmode_oneshot)
-    t6 = time.process_time() - t
-    # t6 = vrep.simxGetLastCmdTime(clientID)
     time.sleep(2)  # Wait two seconds
 
+def end_simulation (clientID):
+    time.sleep(2)  # Wait two seconds
 
+    # Stop simulation
+    vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
 
+    # Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
+    vrep.simxGetPingTime(clientID)
+
+    # Close the connection to V-REP
+    vrep.simxFinish(clientID)
+
+def get_user_angles ():
+    print('Enter the six angles (in radians) for each joint that you would like the robot to achieve. ')
+    theta1 = eval(input('Angle 1: '))
+    theta2 = eval(input('Angle 2: '))
+    theta3 = eval(input('Angle 3: '))
+    theta4 = eval(input('Angle 4: '))
+    theta5 = eval(input('Angle 5: '))
+    theta6 = eval(input('Angle 6: '))
+
+    return (theta1,theta2, theta3, theta4, theta5, theta6)
 
 
 
---
+++ ECE470_Project_Code.py/Project_Assignment3_Polished.py
@@ -0,0 +1,31 @@
+#
+# Project_Assignment3.py
+#
+# Created by Felipe F. and Di Z.
+# 3/12/18
+#
+# Poslished: 3/21/18
+#
+# To show forward kinematics for robot in V_Rep Simulator
+
+
+import vrep
+import math
+import time
+import numpy as np
+import scipy as sp
+from scipy import linalg
+import os.path
+#import sys
+#sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import ECE470_Project_Functions as myfct
+
+(theta1,theta2, theta3, theta4, theta5, theta6) = myfct.get_user_angles ()
+
+(T_1in0, R_1in0, eulerAngles, p_1in0) = myfct.forward_kinematics(theta1,theta2,theta3,theta4,theta5,theta6)
+
+clientID = myfct.start_simulation()
+
+myfct.move_robot(clientID, p_1in0, eulerAngles, theta1, theta2, theta3, theta4, theta5, theta6)
+
+myfct.end_simulation(clientID)
